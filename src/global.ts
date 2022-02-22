2efda2e7561438652d4dc63e58bb33d6a45abbed
ruymon <ruystfrancis@gmail.com>
2022-02-22 19:44:49 -0300

    project config

---
+++ src/global.ts
@@ -0,0 +1,14 @@
+import { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyles = createGlobalStyle`
+  body {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  html {
+    font-size: 62.5%;
+    font-family: 'Ubuntu', sans-serif;
+  }
+`;
---
+++ src/styles/colors.ts
@@ -0,0 +1,26 @@
+export const colors = {
+  primary: {
+    rose: '#FDBAA3',
+    white: '#FFFFFF',
+    midnight: '#3C3D43',
+    creamWhite: '#FFFDFA'
+  },
+  semantic: {
+    red: '#F7685C',
+    green: '#30C58D'
+  },
+  post: {
+    opaqueLightYellow: 'rgba(251, 235, 149, 0.4)',
+    opaqueRedOrange: 'rgba(253, 186, 163, 0.4)',
+    opaqueLilac: 'rgba(182, 165, 203, 0.4)',
+    opaqueGreenCyan: 'rgba(151, 210, 188, 0.6)',
+    opaqueLightCyan: 'rgba(174, 223, 232, 0.6)'
+  },
+  gray: {
+    900: '#000000',
+    800: '#343539',
+    600: '#4D4D4D',
+    400: '#808080',
+    300: '#8C8A97'
+  }
+}
