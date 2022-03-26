952acd81941afb2bdd94f9e88520e86e377e1c4a
ruymon <ruystfrancis@gmail.com>
2022-03-26 01:06:42 -0300

    feat(translation): Translate time formater

--- src/utils/formatTimestamp.ts
+++ src/utils/formatTimestamp.ts
@@ -1,10 +1,21 @@
 import { formatDistance } from 'date-fns';
+import { ptBR, enUS } from 'date-fns/locale'
+import { useTranslation } from 'react-i18next';
 
-export function formatTimestamp(time: number) {
-  console.log(new Date(time));
-  
+interface LanguageDictionary {
+  [key: string]: Locale;
+}
 
-  const formatedTime = formatDistance(new Date(time), new Date(), { addSuffix: true });
+export function formatTimestamp(time: number) { 
+  const { i18n } = useTranslation();
+  const currentLanguage = i18n.language;
+
+  const languageDictionary: LanguageDictionary = {
+    'pt-BR': ptBR,
+    'en-US': enUS,
+  }
+
+  const formatedTime = formatDistance(new Date(time), new Date(), { addSuffix: true, locale: languageDictionary[currentLanguage] });
 
   return formatedTime;
 }
